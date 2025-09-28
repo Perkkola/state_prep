6411945a89d353961a55d58b19755e6c686391cb
Frans Perkkola <fransperkkola@gmail.com>
2025-09-28 19:30:49 +0300

    asd

--- routing_for_QSD/routing_alg.py
+++ routing_for_QSD/routing_alg.py
@@ -3,10 +3,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 from a_star import BasicAStar
+from collections import deque
 
 num_qubits = 3
 num_controls = num_qubits - 1
-pp_terms = [x for x in range(2 ** num_controls + 1, 2 ** num_qubits)]
+max_cnot = num_controls * (2 ** num_controls) #Hypothetical upper bound
+
 
 #Represent the qubit with a floor(log num_qubits) + 1 -bit number.
 #Represent the current phase polynomial term on the qubit by num_qubits -bit number.
@@ -15,66 +17,113 @@ def build_identifier(state):
 
 
 graph = {"vertices": set(), "edges": set(), "neighbors": {}}
-# dist = {}
+pp_states = {}
+dist = deque()
+vertices_with_root_dist = deque()
 starting_state = {}
+state_queue = deque()
 
 for q in range(num_qubits):
     starting_state[q] = 1 << q
 
-id = build_identifier(starting_state)
-graph["vertices"].add(id)
-graph["neighbors"][id] = set()
+for term in range(2 ** num_controls, 2 ** num_qubits):
+    pp_states[term] = []
 
-#Create the search space
-def traverse(state, id):
-    for i in range(num_qubits - 1):
-        cnot_up = state.copy()
-        cnot_up[i] = cnot_up[i] ^ cnot_up[i+1]
+start_id = build_identifier(starting_state)
+state_queue.append((starting_state, 0)) #Include distance from the root
 
-        cnot_up_id = build_identifier(cnot_up)
 
-        if cnot_up_id not in graph["vertices"]:
-            graph["vertices"].add(cnot_up_id)
-            graph["edges"].add((id, cnot_up_id))
 
-            graph["neighbors"][cnot_up_id] = set()
-            traverse(cnot_up, cnot_up_id)
-        else:
-            graph["edges"].add((id, cnot_up_id))
+while True:
+    try:
+        source, source_dist_from_root = state_queue.popleft()
+        source_id = build_identifier(source)
 
-        graph["neighbors"][cnot_up_id].add(id)
-        graph["neighbors"][id].add(cnot_up_id)
+        
+        if source_id in graph["vertices"] or source_dist_from_root > max_cnot / 2:
+            continue
 
-        cnot_down = state.copy()
-        cnot_down[i+1] = cnot_down[i] ^ cnot_down[i+1]
+        graph["vertices"].add(source_id)
+        graph["neighbors"][source_id] = []
+        vertices_with_root_dist.append((source_id, source_dist_from_root))
 
-        cnot_down_id = build_identifier(cnot_down)
+    
+        for i in range(num_qubits - 1):
+            cnot_up = source.copy()
+            cnot_up[i] = cnot_up[i] ^ cnot_up[i+1]
+            cnot_up_id = build_identifier(cnot_up)
+            
 
-        if cnot_down_id not in graph["vertices"]:
-            graph["vertices"].add(cnot_down_id)
-            graph["edges"].add((id, cnot_down_id))
+            if (cnot_up_id, source_id) in graph["edges"]:
+                graph["neighbors"][source_id].append(cnot_up_id)
+            elif source_dist_from_root + 1 <= max_cnot / 2:
+                graph["edges"].add((source_id, cnot_up_id))
+                graph["neighbors"][source_id].append(cnot_up_id)
+                state_queue.append((cnot_up, source_dist_from_root + 1))
+            
+            cnot_down = source.copy()
+            cnot_down[i+1] = cnot_down[i] ^ cnot_down[i+1]
+            cnot_down_id = build_identifier(cnot_down)
+ 
+            if (cnot_down_id, source_id) in graph["edges"]:
+                graph["neighbors"][source_id].append(cnot_down_id)
+            elif source_dist_from_root + 1 <= max_cnot / 2:
+                graph["edges"].add((source_id, cnot_down_id))
+                graph["neighbors"][source_id].append(cnot_down_id)
+                state_queue.append((cnot_down, source_dist_from_root + 1))
 
-            graph["neighbors"][cnot_down_id] = set()
-            traverse(cnot_down, cnot_down_id)
-        else:
-            graph["edges"].add((id, cnot_down_id))
+            if source[i] >= 2 ** num_controls and source[i] < 2 ** num_qubits: pp_states[source[i]].append((source_id, source_dist_from_root))
+        if source[num_qubits - 1] >= 2 ** num_controls and source[num_qubits - 1] < 2 ** num_qubits: pp_states[source[num_qubits - 1]].append((source_id, source_dist_from_root))
+    except Exception as e:
+        # print(e)
+        break
 
-        graph["neighbors"][cnot_down_id].add(id)
-        graph["neighbors"][id].add(cnot_down_id)
+# AStar = BasicAStar(graph["neighbors"])
 
-traverse(starting_state, id)
+# for i in range(2 ** num_controls, 2 ** num_qubits):
+#     for a in pp_states[i]:
+#         for j in range(2 ** num_controls + (i % (2 ** num_controls)) + 1, 2 ** num_qubits):
+#             for b in pp_states[j]:
+#                 id_a, a_dist_from_root = a
+#                 id_b, b_dist_from_root = b
 
-path = BasicAStar(graph["neighbors"]).astar(273, 305)
+#                 if a_dist_from_root + b_dist_from_root >= max_cnot:
+#                     continue
 
-for term in pp_terms:
-    for q in num_qubits:
-        for v in graph["vertices"]:
-            pass
-print(list(path))
-exit()
+#                 path = AStar.astar(id_a, id_b)
+
+#                 if len(list(path)) - 1 + a_dist_from_root + b_dist_from_root > max_cnot:
+#                     continue
+
+#                 dist.append((a, b, len(list(path)) - 1))
+
+# for term in pp_terms:
+#     for q in num_qubits:
+#         for v in graph["vertices"]:
+#             pass
+
+
+graph["vertices"] = list(graph["vertices"])
+graph["edges"] = list(graph["edges"])
 print(len(graph["vertices"]))
 print(len(graph["edges"]))
-print(graph["neighbors"][273])
+# print(graph["neighbors"][273])
+print(len(dist))
+
+print(pp_states)
+exit()
+s = 0
+
+for key in pp_states.keys():
+    s += len(pp_states[key])
+
+print(s)
+
+
+# print(vertices_with_root_dist)
+# print(pp_states[2 ** num_controls])
+# print(pp_states[2 ** num_controls + 1])
+# exit()
 
 
 
