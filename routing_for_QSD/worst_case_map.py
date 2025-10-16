c653a5de0b5995ba994c301349cba5141ed54c13
Frans Perkkola <fransperkkola@gmail.com>
2025-10-16 20:07:42 +0300

    cracked the algorithm

--- routing_for_QSD/worst_case_map.py
+++ routing_for_QSD/worst_case_map.py
@@ -10,180 +10,208 @@ num_controls = num_qubits - 1
 
 assert num_controls >= 1
 
-pp_terms = set([x for x in range(2 ** num_controls, 2 ** num_qubits)])
-# discovered_pp_terms = set([2 ** num_controls])
 
-len_pp_terms = 2 ** num_controls
 
-# state = {q: 1 << q for q in range(num_qubits)}
-# gate_queue = deque()
-# gate_queue.append("RZ")
+def grey_code(dist, half):
+    upper_bound = (2 ** (dist - 1) + 1) if half else 2 ** dist + 1
 
-def grey_code(g):
-    # state = s.copy()
-    gates = g.copy()
-
-    for i in range(1, len_pp_terms + 1):
+    for i in range(1, upper_bound):
         highest_index_diff = 0
-        for j in range(num_controls):
+        for j in range(dist):
             if (i >> j) & 1 != ((i - 1) >> j) & 1: highest_index_diff = j + 1
-        # state[num_controls] = state[num_controls - highest_index_diff] ^ state[num_controls]
-        gates.append("RZ")
-        gates.append((num_controls - highest_index_diff, num_controls))
+        grey_gate_queue.append("RZ")
+        grey_gate_queue.append((dist - highest_index_diff, dist))
 
-    return gates
+def get_long_range_grey_gates(dist, half):
+    global grey_gate_queue
+    grey_gate_queue = deque()
 
-def cancel_or_append(cnot):
+    grey_code(dist, half)
+
+    long_range_gates = []
+    for gate in grey_gate_queue:
+        if gate == "RZ": continue
+
+        if gate[1] - gate[0] > 1: long_range_gates.append(gate)
+    
+    return long_range_gates
+
+def cancel_or_append(cnot, size):
     global state
     global gate_queue
     global discovered_pp_terms
+    global first_half_terms
+    global first_half
 
-    barrier_found = False
-    barrier_type = "U"
     prev_gate = gate_queue.pop()
-
-    if prev_gate == "Barrier U":
-        barrier_type = "U"
-        barrier_found = True
-        prev_gate = gate_queue.pop()
-
-    if prev_gate == "Barrier C":
-        barrier_type = "C"
-        barrier_found = True
-        prev_gate = gate_queue.pop()
-
 
     if prev_gate != cnot:
         gate_queue.append(prev_gate)
-        if barrier_found: gate_queue.append(f"Barrier {barrier_type}")
         gate_queue.append(cnot)
-    elif barrier_found: gate_queue.append(f"Barrier {barrier_type}")
     
-    if cnot[1] == num_controls and state[num_controls] in pp_terms and state[num_controls] not in discovered_pp_terms:
-        discovered_pp_terms.add(state[num_controls])
+    if cnot[1] == size and state[size] in pp_terms and state[size] not in discovered_pp_terms:
+        discovered_pp_terms.add(state[size])
+        if not first_half: 
+            first_half_terms.append(state[size])
         gate_queue.append("RZ")
 
-def long_range_cnot(dist):
+def long_range_cnot(dist, size):
     global state
-    offset = num_controls - dist
+    offset = size - dist
     
     for i in range(dist):
         i_f = i + offset
         state[i_f + 1] = state[i_f + 1] ^ state[i_f]
-        cancel_or_append((i_f, i_f+1))
+        cancel_or_append((i_f, i_f+1), size)
     for j in range(dist - 1, 0, -1):
         j_f = j + offset
         state[j_f] = state[j_f] ^ state[j_f - 1]
-        cancel_or_append((j_f - 1, j_f))
+        cancel_or_append((j_f - 1, j_f), size)
     for k in range(1, dist):
         k_f = k + offset
         state[k_f + 1] = state[k_f + 1] ^ state[k_f]
-        cancel_or_append((k_f, k_f + 1))
+        cancel_or_append((k_f, k_f + 1), size)
     for l in range(dist - 1, 1, -1):
         l_f = l + offset
         state[l_f] = state[l_f] ^ state[l_f - 1]
-        cancel_or_append((l_f - 1, l_f))
+        cancel_or_append((l_f - 1, l_f), size)
 
-def reverse_long_range_cnot(dist):
+def reverse_long_range_cnot(dist, size):
     global state
-    offset = num_controls - dist
+    offset = size - dist
 
     for l in reversed(range(dist - 1, 1, -1)):
         l_f = l + offset
         state[l_f] = state[l_f] ^ state[l_f - 1]
-        cancel_or_append((l_f - 1, l_f))
+        cancel_or_append((l_f - 1, l_f), size)
     for k in reversed(range(1, dist)):
         k_f = k + offset
         state[k_f + 1] = state[k_f + 1] ^ state[k_f]
-        cancel_or_append((k_f, k_f + 1))
+        cancel_or_append((k_f, k_f + 1), size)
     for j in reversed(range(dist - 1, 0, -1)):
         j_f = j + offset
         state[j_f] = state[j_f] ^ state[j_f - 1]
-        cancel_or_append((j_f - 1, j_f))
+        cancel_or_append((j_f - 1, j_f), size)
     for i in reversed(range(dist)):
         i_f = i + offset
         state[i_f + 1] = state[i_f + 1] ^ state[i_f]
-        cancel_or_append((i_f, i_f + 1))
+        cancel_or_append((i_f, i_f + 1), size)
 
-def route_multiplexor(long_range_grey_gates):
+def long_range_leg(dist, state, size):
+    offset = size - dist
+    for i in range(dist):
+        i_f = i + offset
+        state[i_f + 1] = state[i_f + 1] ^ state[i_f]
+
+    return state
+
+def reverse_long_lange_leg(dist, state, size):
+    dist = dist - 1
+    return long_range_cnot(dist, state, size)
+
+def route_multiplexor():
     global state
     global gate_queue
     global discovered_pp_terms
+    global first_half_terms
+    global first_half
+    global pp_terms
 
-    num_configurations = 2 ** 8
+    first_half = True
+    first_half_terms = [2, 3]
     
-    for conf in range(num_configurations):
-        discovered_pp_terms = set([2 ** num_controls])
-        state = {q: 1 << q for q in range(num_qubits)}
+
+    for i in range(num_controls):
+        first_half = True
+        long_range_gates = get_long_range_grey_gates(i + 1, True)
+        bit_map = deque()
+        
+        pp_terms = set([x for x in range(2 ** (i + 1), 2 ** (i + 2))])
+        discovered_pp_terms = set([2 ** (i + 1)])
+        state = {q: 1 << q for q in range(i + 2)}
         gate_queue = deque()
         gate_queue.append("RZ")
 
+        long_range_cnot(1, i + 1)
+        for j, gate in enumerate(long_range_gates[:-1]):
+            next_term = first_half_terms[2:][j * 2]
+            test_state = long_range_leg(gate[1] - gate[0], state.copy(), i + 1)
 
-        long_range_cnot(1)
-        gate_queue.append("Barrier C")
-        long_range_cnot(2)
-        gate_queue.append("Barrier C")
-        long_range_cnot(3)
-        gate_queue.append("Barrier C")
-        reverse_long_range_cnot(2)
-        gate_queue.append("Barrier U")
-        long_range_cnot(4)
-        gate_queue.append("Barrier C")
-        long_range_cnot(2)
-        gate_queue.append("Barrier C")
-        reverse_long_range_cnot(3)
-        gate_queue.append("Barrier U")
-        long_range_cnot(2)
-        gate_queue.append("Barrier C")
-        long_range_cnot(5)
-        gate_queue.append("Barrier C")
-        reverse_long_range_cnot(2)
-        gate_queue.append("Barrier U")
-        long_range_cnot(3)
-        gate_queue.append("Barrier C")
-        reverse_long_range_cnot(2)
-        gate_queue.append("Barrier U")
+            if test_state[i + 1] == next_term:
+                long_range_cnot(gate[1] - gate[0], i + 1)
+                bit_map.append(1)
+            else:
+                reverse_long_range_cnot(gate[1] - gate[0], i + 1)
+                bit_map.append(0)
+
+        first_half = False
+        long_range_cnot(i + 1, i + 1)
+        bit_map.append(1)
+        print(bit_map)
+        for gate in long_range_gates:
+            if bit_map.popleft() == 0 or i + 1 == 2:
+                long_range_cnot(gate[1] - gate[0], i + 1)
+            else:
+                reverse_long_range_cnot(gate[1] - gate[0], i + 1)
+
+        long_range_cnot(1, i + 1)
+
+        print((i+1, first_half_terms))
+        first_half_terms = [x * 2 for x in first_half_terms]
+
+    
         
-        long_range_cnot(1)
 
     circuit_length = len([gate for gate in gate_queue if gate != "RZ" and gate != "Barrier C" and gate != "Barrier U"])
     print(f"Found {len(discovered_pp_terms)}/{len(pp_terms)} phase polynomial terms.")
     print(f"Circuit length: {circuit_length}")
+    draw_circuit(0, gate_queue)
     return gate_queue
 
-def find_optimal(start, stop, long_range_gates, core_index, return_dict):
+def find_optimal(long_range_gates):
     global state
     global gate_queue
     global discovered_pp_terms
+    global first_half_terms
+    global first_half
+    global pp_terms
+
+    pp_terms = set([x for x in range(2 ** num_controls, 2 ** num_qubits)])
+    first_half = True
+    first_half_terms = [2, 3]
 
     optimal_circuits = []
     optimal_circuit_len = (2 ** num_qubits) * (2 ** num_qubits)
 
-    for conf in range(start, stop):
+    num_configurations = 2 ** 16
+
+    for n in range(num_configurations):
+        conf = int(f"1{(n >> 0) & 1}011{(n >> 1) & 1}010{(n >> 2) & 1}011{(n >> 3) & 1}011{(n >> 4) & 1}011{(n >> 5) & 1}010{(n >> 6) & 1}011{(n >> 7) & 1}010{(n >> 8) & 1}011{(n >> 9) & 1}010{(n >> 10) & 1}011{(n >> 11) & 1}011{(n >> 12) & 1}011{(n >> 13) & 1}010{(n >> 14) & 1}011{(n >> 15) & 1}011010001010100010001000101010001011011101010111011010001011011011", 2)
+
         discovered_pp_terms = set([2 ** num_controls])
         state = {q: 1 << q for q in range(num_qubits)}
         gate_queue = deque()
         gate_queue.append("RZ")
 
-        long_range_cnot(1)
+        long_range_cnot(1, num_controls)
         gate_queue.append("Barrier C")
-        for i, gate in enumerate(long_range_gates[:int(len(long_range_gates) / 2)]):
+        for i, gate in enumerate(long_range_gates):
             if (conf >> i) & 1 == 1:
-                long_range_cnot(gate[1] - gate[0])
+                long_range_cnot(gate[1] - gate[0], num_controls)
                 gate_queue.append("Barrier C")
             else:
-                reverse_long_range_cnot(gate[1] - gate[0])
+                reverse_long_range_cnot(gate[1] - gate[0], num_controls)
                 gate_queue.append("Barrier U")
 
-        for i, gate in enumerate(long_range_gates[:int(len(long_range_gates) / 2)]):
+        for i, gate in enumerate(long_range_gates):
             if (conf >> i) & 1 == 0:
-                long_range_cnot(gate[1] - gate[0])
+                long_range_cnot(gate[1] - gate[0], num_controls)
                 gate_queue.append("Barrier C")
             else:
-                reverse_long_range_cnot(gate[1] - gate[0])
+                reverse_long_range_cnot(gate[1] - gate[0], num_controls)
                 gate_queue.append("Barrier U")
 
-        long_range_cnot(1)
+        long_range_cnot(1, num_controls)
 
         # draw_circuit(gate_queue)
         if len(discovered_pp_terms) != len(pp_terms): continue
@@ -199,7 +227,7 @@ def find_optimal(start, stop, long_range_gates, core_index, return_dict):
             optimal_circuits.append(gate_queue.copy())
         else: continue
 
-    return_dict[core_index] = (optimal_circuits, optimal_circuit_len)
+    return optimal_circuits, optimal_circuit_len
 
 def draw_circuit(index, gates):
     qc = QuantumCircuit(num_qubits)
@@ -215,62 +243,25 @@ def draw_circuit(index, gates):
         else:
             qc.cx(gate[0], gate[1])
     
-    # fig = qc.draw(output="mpl", interactive=True, filename=f"./circuits/grey/optimal_circuit_{num_controls}-ctrl_{index}.png")
-    fig = qc.draw(output="mpl", interactive=True)
+    fig = qc.draw(output="mpl", interactive=True, filename=f"./circuits/cnot_at_end/optimal_circuit_{num_controls}-ctrl_{index}.png")
+    # fig = qc.draw(output="mpl", interactive=True)
     plt.show()
 
 
 if __name__ == "__main__":
-    # gates = route_multiplexor()
-    # draw_circuit(0, gates)
+
+
+    long_range_gates = get_long_range_grey_gates(num_controls, True)
+
+    # route_multiplexor()
     # exit()
+    optimal_circuits, optimal_circuit_len = find_optimal(long_range_gates)
 
-    grey_gate_queue = deque()
-    grey_gates = grey_code(grey_gate_queue)
 
-    long_range_gates = []
-    for gate in grey_gates:
-        if gate == "RZ": continue
-
-        if gate[1] - gate[0] > 1: long_range_gates.append(gate)
-
-    num_configurations = 2 ** (len(long_range_gates) / 2)
-
-    # route_multiplexor(long_range_gates)
-    # exit()
-
-    manager = multiprocessing.Manager()
-    return_dict = manager.dict()
-    jobs = []
-    cores = multiprocessing.cpu_count()
-
-    for index in range(cores):
-        start = int(math.ceil(num_configurations / cores) * index)
-        stop = int(min(math.ceil(num_configurations / cores) * (index + 1), num_configurations))
-        p = multiprocessing.Process(target=find_optimal, args=(start, stop, long_range_gates, index, return_dict))
-        # print(f"[{start}, {stop}]")
-        jobs.append(p)
-        p.start()
-    for proc in jobs:
-        proc.join()
-
-    optimal_circuits = []
-    optimal_circuit_len = (2 ** num_qubits) * (2 ** num_qubits)
-
-    for item in return_dict.items():
-        circuits = item[1][0]
-        circuit_len = item[1][1]
-
-        if circuit_len < optimal_circuit_len:
-            optimal_circuits = circuits
-            optimal_circuit_len = circuit_len
-        elif circuit_len == optimal_circuit_len:
-            optimal_circuits.extend(circuits)
-     
 
     print(f"Long range cnots in circuit: {len(long_range_gates)}")
     print(f"Found {len(optimal_circuits)} optimal circuits.")
     print(f"Optimal circuit length: {optimal_circuit_len}")
     for index, circuit in enumerate(optimal_circuits):
-        draw_circuit(index, circuit)
+        draw_circuit(0, circuit)
 
